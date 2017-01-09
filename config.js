167cc1698a305d51de77e235736dcff120c6fd11
marioosh <marioosh.net@gmail.com>
2017-01-09 15:00:18 +0100

    ssl

--- config.js
+++ config.js
@@ -2,5 +2,11 @@ module.exports = {
 	basicAuth: {
 		username: 'username',
 		password: 'password'
-	}
+	},
+    tls: {
+    	active: true,
+    	key: '/etc/ssl/apache2/server.key',
+    	cert: '/etc/ssl/apache2/server.crt',
+    	ciphers: 'ECDHE-RSA-AES256-SHA:AES256-SHA:RC4-SHA:RC4:HIGH:!MD5:!aNULL:!EDH:!AESGCM'
+    }
 }
